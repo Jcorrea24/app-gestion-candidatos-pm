d7a3edf04e3366daba3fd5552b70eaef525dae43
Julio Correa <jcorrea24@gmail.com>
2026-03-17 20:27:19 -0500

    (chore): Instalación y configuración de la dependencia react-router-dom en el archivo main.jsx

---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,26 @@
+import App from '../App.jsx'
+import Login from '../pages/Login.jsx'
+import Register from '../pages/Register.jsx'
+import Dashboard from '../pages/Dashboard.jsx'
+
+
+let routerApp = [
+    {
+        path: "/",
+        element:<App />
+    },
+        {
+        path: "/login",
+        element: <Login />
+    },
+     {
+        path: "/register",
+        element: <Register />
+    },
+     {
+        path: "/dashboard",
+        element: <Dashboard />
+    },
+]
+
+export default routerApp;
