9546f176ef3a11cb9961dd13e8c84b7e36970bc5
Julio Correa <jcorrea24@gmail.com>
2026-03-17 20:59:37 -0500

    (chore): definición de rutas principales para la comunicación con la API generadas desde el back

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const URL_BASE = 'http://localhost:8080/';
+export let endpoints = {
+    users: `${URL_BASE}users`,
+    offers: `${URL_BASE}offers`,
+    candidates: `${URL_BASE}candidates`
+ 
+
+}
